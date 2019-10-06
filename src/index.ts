7199a4e60e11e24dd4b866ca685adcd4465376d7
Jan Andersson <jan.andersson@gmail.com>
2019-10-06 22:15:04 +0200

    Update index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-// zip, splitEvery, groupBy
-
+export { default as compose } from "./compose"
 export { default as prop } from "./prop"
 export { default as propEq } from "./propEq"
-export { default as compose } from "./compose"
+export { default as splitEvery } from "./splitEvery"
